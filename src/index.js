baf53e7587f9c3aaa5acaa8acc432237787b62ee
Raulgm111 <raul.garciamoratinos@outlook.com>
2022-11-23 21:37:46 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Auth0Provider} from '@auth0/auth0-react'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider domain='raulgarcia.eu.auth0.com' clientId='WBcxmSSnkQPe7nee9rqCaVsSrEtY9TqI' 
+    redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
